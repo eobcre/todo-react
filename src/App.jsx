9c156d320daed61c19d0d7a430adea90e904bab3
eobcre <eobcre@gmail.com>
2023-05-04 16:04:13 -0400

    add button logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import { useState } from 'react';
 import { todoSample } from './todoSample';
+import { nanoid } from 'nanoid';
 
 const App = () => {
   const [todos, setTodos] = useState(todoSample);
   const [inputTodo, setInputTodo] = useState('');
 
   // input settings logic
-  const handleChangeAdd = (e) => {
+  const handleInputChange = (e) => {
     setInputTodo(e.target.value);
+  };
+
+  // add button logic
+  const handleClickAdd = () => {
+    const newTodo = {
+      id: nanoid(),
+      content: inputTodo,
+    };
+    // spread array (...todos), add new todo (newTodo)
+    setTodos([...todos, newTodo]);
+    // clear input
+    setInputTodo('');
   };
 
   return (
     <div>
       <h2>List</h2>
-      <input type='text' value={inputTodo} onChange={handleChangeAdd} />
-      <button>Add</button>
+      <input type='text' value={inputTodo} onChange={handleInputChange} />
+      <button onClick={handleClickAdd}>Add</button>
       {todos.map(({ id, content }) => {
         return (
           <div key={id}>
