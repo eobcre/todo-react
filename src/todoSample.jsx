741fdb0d449f347de2a807b887c915d4773f2e23
eobcre <eobcre@gmail.com>
2023-05-03 22:25:48 -0400

    created default default todos data

---
+++ src/todoSample.jsx
@@ -0,0 +1,16 @@
+import { nanoid } from 'nanoid';
+
+export const todoSample = [
+  {
+    id: nanoid,
+    content: 'Study JavaScript',
+  },
+  {
+    id: nanoid,
+    content: 'Study React.js',
+  },
+  {
+    id: nanoid,
+    content: 'Study TypeScript',
+  },
+];
