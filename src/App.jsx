e9c835b46cd046fa0a4fe6284a9b7550350dd599
eobcre <eobcre@gmail.com>
2023-05-03 22:26:54 -0400

    created todo list layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { useState } from 'react';
+import { todoSample } from './todoSample';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const [todos, setTodos] = useState(todoSample);
+  return (
+    <div>
+      <h2>List</h2>
+      <input type='text' />
+      <button>Add</button>
+      {todos.map(({ id, content }) => {
+        return (
+          <div key={id}>
+            <span>{content}</span>
+            <button>Done</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default App;
