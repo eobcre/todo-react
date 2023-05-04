6d70c556927f7ecb13d4180c82b3efdb7d1ce189
eobcre <eobcre@gmail.com>
2023-05-04 12:39:33 -0400

    input settings logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import { todoSample } from './todoSample';
 
 const App = () => {
   const [todos, setTodos] = useState(todoSample);
+  const [inputTodo, setInputTodo] = useState('');
+
+  // input settings logic
+  const handleChangeAdd = (e) => {
+    setInputTodo(e.target.value);
+  };
+
   return (
     <div>
       <h2>List</h2>
-      <input type='text' />
+      <input type='text' value={inputTodo} onChange={handleChangeAdd} />
       <button>Add</button>
       {todos.map(({ id, content }) => {
         return (
